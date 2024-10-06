9ca8f8718d83d8f5808933abb37863207b58b1e1
alikpashaevi <alikpashaev06@gmail.com>
2024-10-06 17:22:47 +0400

    login and register with hashed password

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import mongoose from "mongoose";  // Corrected the mongoose import
 import cors from "cors";
 import env from "dotenv";
 import connectDb from "./db.js";
+import User from "./models/user.js";
+import bcrypt from "bcrypt";
+
+env.config();
 
 const app = express();
 const port = 3000;
@@ -11,13 +15,61 @@ const saltRounds = 10;
 // Middleware
 app.use(cors());
 app.use(express.json());
-env.config();
+
 
 // Connect to MongoDB
 connectDb();
 
 // Routes
 
+//Login
+app.post("/api/login", async (req, res) => {
+  const { username, password } = req.body;
+  console.log(username, password);
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: "Invalid password" });
+    }
+    res.json({ message: "Login successful" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+}
+)
+
+// Register a new user
+app.post("/api/register", async (req, res) => {
+  const { username, password } = req.body;
+  console.log(username, password);
+
+  try {
+    // Check if the user already exists
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    // Create a new user with the hashed password
+    const user = new User({ username, password: hashedPassword });
+
+    // Save the user to the database
+    await user.save();
+
+    res.json({ msg: 'User registered successfully' });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+})
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- models/user.js
+++ models/user.js
@@ -1,31 +1,35 @@
 import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
-  clerkId: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
+  // id: {
+  //   type: String,
+  //   required: true,
+  //   unique: true,
+  // },
+  // email: {
+  //   type: String,
+  //   required: true,
+  //   unique: true,
+  // },
   username: {
     type: String,
     required: true,
     unique: true,
   },
-  photo: {
+  password: {
     type: String,
     required: true,
   },
-  firstName: {
-    type: String,
-  },
-  lastName: {
-    type: String,
-  },
+  // photo: {
+  //   type: String,
+  //   required: true,
+  // },
+  // firstName: {
+  //   type: String,
+  // },
+  // lastName: {
+  //   type: String,
+  // },
   planId: {
     type: Number,
     default: 1,
